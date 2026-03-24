54806e7ec74c8fe0af254c0333a274b92271f763
frenchcast1234 <156002886+frenchcast1234@users.noreply.github.com>
2026-03-24 07:43:07 +0100

    Add new games to the data array
    Added new games to the data array including Slalom Canoe, Hurdles, Cricket, Football, Basketball, and Garden Gnome.

--- data/games.js
+++ data/games.js
@@ -1,8 +1,14 @@
-export const data = [
+const data = [
     { name: "Chrome Dino", url: "chrome://dino", tags: ["dino"] }, 
     { name: "Snake", url: "https://www.google.com/search?q=snake", tags: ["snake"] },
     { name: "Tic Tac Toe", url: "https://www.google.com/search?q=tic%20tac%20toe", tags: ["tic", "tac", "toe"] },
     { name: "Pacman", url: "https://www.google.com/search?q=pacman", tags: ["pacman"] },
     { name: "Minesweeper", url: "https://www.google.com/search?q=minesweeper", tags: ["minesweeper"] },
     { name: "Block Breaker", url: "https://www.google.com/search?q=block%20breaker", tags: ["block", "breaker", "atari", "breakout"] },
+    { name: "Slalom Canoe", url: "https://doodles.google/doodle/slalom-canoe-2012/", tags: ["slalom", "canoe"] },
+    { name: "Hurdles", url: "https://doodles.google/doodle/hurdles-2012/", tags: ["hurdles"] },
+    { name: "Cricket", url: "https://www.google.com/search?q=cricket%20doodle", tags: ["cricket"] },
+    { name: "Football", url: "https://doodles.google/doodle/soccer-2012/", tags: ["football", "soccer"] },
+    { name: "Basketball", url: "https://doodles.google/doodle/basketball-2012/", tags: ["basket", "basketball"] },
+    { name: "Garden Gnome", url: "https://www.google.com/search?q=garden%20gnome", tags: ["garden gnomes", "garden", "gnomes"] }
 ];
