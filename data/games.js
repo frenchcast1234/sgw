7c46a949de827fb3996da4a9d1b1c8b3ae22f800
frenchcast1234 <156002886+frenchcast1234@users.noreply.github.com>
2026-03-24 06:42:27 +0100

    Remove comment from Chrome Dino entry

--- data/games.js
+++ data/games.js
@@ -1,5 +1,5 @@
 export const data = [
-    { name: "Chrome Dino", url: "chrome://dino", tags: ["dino"] }, // Tags for the future
+    { name: "Chrome Dino", url: "chrome://dino", tags: ["dino"] }, 
     { name: "Snake", url: "https://www.google.com/search?q=snake", tags: ["snake"] },
     { name: "Tic Tac Toe", url: "https://www.google.com/search?q=tic%20tac%20toe", tags: ["tic", "tac", "toe"] },
     { name: "Pacman", url: "https://www.google.com/search?q=pacman", tags: ["pacman"] },
