49a99e517149e4f65d40d2e191cfb1b05815f012
frenchcast1234 <156002886+frenchcast1234@users.noreply.github.com>
2026-03-24 06:17:30 +0100

    Add new games and update tags in games.js

--- data/games.js
+++ data/games.js
@@ -1,6 +1,8 @@
 export const data = [
     { name: "Chrome Dino", url: "chrome://dino", tags: ["dino"] }, // Tags for the future
     { name: "Snake", url: "https://www.google.com/search?q=snake", tags: ["snake"] },
-    { name: "Tic Tac Toe", url: "https://www.google.com/search?q=tic%20tac%20toe", tags: ["snake"] },
-    { name: "Pacman", url: "https://www.google.com/search?q=pacman", tags: ["pacman"] }
+    { name: "Tic Tac Toe", url: "https://www.google.com/search?q=tic%20tac%20toe", tags: ["tic", "tac", "toe"] },
+    { name: "Pacman", url: "https://www.google.com/search?q=pacman", tags: ["pacman"] },
+    { name: "Minesweeper", url: "https://www.google.com/search?q=minesweeper", tags: ["minesweeper"] },
+    { name: "Block Breaker", url: "https://www.google.com/search?q=block%20breaker", tags: ["block", "breaker", "atari", "breakout"] },
 ];
