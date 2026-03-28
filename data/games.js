47a1206414a4b7c96e242b0ec6d09f08a0c1a359
frenchcast1234 <156002886+frenchcast1234@users.noreply.github.com>
2026-03-28 15:18:13 +0100

    Update games.js

--- data/games.js
+++ data/games.js
@@ -11,5 +11,6 @@ export const data = [
     { name: "Football", url: "https://doodles.google/doodle/soccer-2012/", tags: ["football", "soccer"] },
     { name: "Basketball", url: "https://doodles.google/doodle/basketball-2012/", tags: ["basket", "basketball"] },
     { name: "Garden Gnome", url: "https://www.google.com/search?q=garden%20gnome", tags: ["garden gnomes", "garden", "gnomes"] },
-    { name: "Tuiles colorées", url: "https://www.google.com/search?q=tuiles+colorees+google", tags: ["tuiles colorées", "tuile", "colorées"] }
+    { name: "Tuiles colorées", url: "https://www.google.com/search?q=tuiles+colorees+google", tags: ["tuiles colorées", "tuile", "colorées"] },
+    { name: "Emoji Kitchen", url: "https://www.google.com/search?q=emoji+kitchen", tags: ["emoji kitchen", "emoji", "kitchen"] }
 ];
